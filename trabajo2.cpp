98a739ddbfb7a4695e9ff052e9e9e37f086c6abc
iairalejandro <101744230+iairalejandro@users.noreply.github.com>
2024-01-22 09:12:33 -0500

    Update trabajo2.cpp

--- trabajo2.cpp
+++ trabajo2.cpp
@@ -23,3 +23,46 @@ int main() {
 
 }
 
+
+
+#include <iostream>
+#include <cstdlib> // Para la función rand()
+#include <ctime>   // Para la función time()
+
+const int FILAS = 10;
+const int COLUMNAS = 10;
+
+char generarLetraAleatoria() {
+    // Generar un número aleatorio entre 0 y 25 para las letras del alfabeto
+    int numeroAleatorio = rand() % 26;
+    
+    // Si el número aleatorio es par, generar una letra mayúscula; de lo contrario, generar una letra minúscula
+    char letra = (numeroAleatorio % 2 == 0) ? 'A' + numeroAleatorio : 'a' + numeroAleatorio;
+
+    return letra;
+}
+
+int main() {
+    // Inicializar la semilla para la generación de números aleatorios
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+    // Declarar la matriz
+    char matriz[FILAS][COLUMNAS];
+
+    // Llenar la matriz con letras aleatorias
+    for (int i = 0; i < FILAS; ++i) {
+        for (int j = 0; j < COLUMNAS; ++j) {
+            matriz[i][j] = generarLetraAleatoria();
+        }
+    }
+
+    // Imprimir la matriz
+    for (int i = 0; i < FILAS; ++i) {
+        for (int j = 0; j < COLUMNAS; ++j) {
+            std::cout << matriz[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
