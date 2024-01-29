b6d3ec47c42ad6cb69c9c20ba30c38881241d12f
iairalejandro <101744230+iairalejandro@users.noreply.github.com>
2024-01-29 12:54:32 -0500

    Update juego-gusano(beta).cpp

--- juego-gusano(beta).cpp
+++ juego-gusano(beta).cpp
@@ -5,157 +5,6 @@ using namespace std;
 
 class cFruta {
 public:
-	int pos_i;
-	int pos_j;
-	char car_fruta;
-public:
-	cFruta() {
-		pos_i = -1, pos_j = -1;
-		car_fruta = '&';
-	}
-
-	void crearFruta(int num_f, int num_c) {
-		pos_i = std::rand() % num_f;
-		pos_j = std::rand() % num_c;
-	}
-
-};
-
-class cGusano {
-public:
-	int* pos_i;
-	int* pos_j;
-	int tam;
-	char simbolo;
-public:
-	cGusano() {
-		pos_i = nullptr;
-		pos_j = nullptr;
-		tam = 0;
-		simbolo = '@';
-	}
-
-	void crearGusano(int num_f, int num_c) {
-		if (tam == 0) {
-			pos_i = new int[1];
-			pos_j = new int[1];
-			pos_i[0] = std::rand() % num_f;
-			pos_j[0] = std::rand() % num_c;
-			tam = 1;
-		}
-	}
-
-	bool isRun = true; char tecla = 'q';
-	while (isRun) {
-		std::cout << "Ingrese una tecla(W,A,S,D) y 'Q' para salir: ";
-		std::cin >> tecla;
-		switch (tecla) {
-			case 'w';
-			case 'W': std::cout << "Apreto W";
-				break;
-			case 'a';
-			case 'A': std::cout << "Apreto A";
-				break;
-			case 's';
-			case 'S': std::cout << "Apreto S";
-				break;
-			case 'd';
-			case 'D': std::cout << "Apreto D";
-				break;
-			case 'q';
-			case 'Q': std::cout << "Apreto Q";
-				break;
-			default: std::cout << "Ingrese una tecla valida...";
-
-
-
-		}
-	}
-};
-
-class cTablero {
-public:
-	int f;
-	int c;
-	char** miMatriz;
-	cFruta miFruta;
-	cGusano miGusano;
-
-public:
-	cTablero() {
-		f = 0;
-		c = 0;
-		miMatriz = nullptr;
-
-	}
-
-	cTablero(int in_f, int in_c) {
-		f = in_f; c = in_c;
-		miMatriz = new char* [f];
-		for (int i = 0; i < f; i++) {
-			miMatriz[i] = new char[c];
-		}
-	}
-
-	void llenarMatriz(char in_caracter) {
-		for (int i = 0; i < f; i++) {
-			for (int j = 0; j < c; j++) {
-				miMatriz[i][j] = in_caracter;
-			}
-		}
-	}
-
-	void printMatriz() {
-		for (int i = 0; i < f; i++) {
-			for (int j = 0; j < c; j++) {
-				std::cout << miMatriz[i][j] << " ";
-			}
-			std::cout << std::endl;
-		}
-		return;
-	}
-
-	void generarFruta() {
-		miFruta.crearFruta(f,c);
-		miMatriz[miFruta.pos_i][miFruta.pos_j] = miFruta.car_fruta;
-	}
-
-	void generarGusano() {
-		miGusano.crearGusano(f, c);
-		miMatriz[miGusano.pos_i[0]][miGusano.pos_j[0]] = miGusano.simbolo;
-	}
-
-};
-
-
-
-int main()
-{
-	cTablero miTablero(10, 10);
-	miTablero.llenarMatriz('_');
-	miTablero.printMatriz();
-	std::cout << std::endl;
-	miTablero.generarFruta();
-	miTablero.printMatriz();
-	std::cout << std::endl;
-	miTablero.generarGusano();
-	miTablero.printMatriz();
-
-	return 0;
-
-}
-
-//.......................................................................................nueva version
-
-
-
-#include <iostream>
-#include <stdlib.h>
-
-using namespace std;
-
-class cFruta {
-public:
     int pos_i;
     int pos_j;
     char car_fruta;
@@ -170,7 +19,21 @@ public:
         pos_j = std::rand() % num_c;
     }
 
+
 };
+
+
+// nueva clase frutaaaa
+/*
+
+class cFrutaEspecial{
+
+
+
+};
+
+*/
+
 
 class cGusano {
 public:
@@ -201,31 +64,40 @@ public:
         int nueva_pos_j = pos_j[0];
 
         switch (direccion) {
-            case 'w':
-            case 'W':
-                nueva_pos_i--;
-                break;
-            case 'a':
-            case 'A':
-                nueva_pos_j--;
-                break;
-            case 's':
-            case 'S':
-                nueva_pos_i++;
-                break;
-            case 'd':
-            case 'D':
-                nueva_pos_j++;
-                break;
-            default:
-                cout << "NO ES UNA TECLA VALIDA, INTENTELO NUEVAMENTE..." << endl;
-                return;
+        case 'w':
+        case 'W':
+            nueva_pos_i--;
+            break;
+        case 'a':
+        case 'A':
+            nueva_pos_j--;
+            break;
+        case 's':
+        case 'S':
+            nueva_pos_i++;
+            break;
+        case 'd':
+        case 'D':
+            nueva_pos_j++;
+            break;
+        default:
+            cout << "NO ES UNA TECLA VALIDA, INTENTELO NUEVAMENTE..." << endl;
+            return;
         }
 
         if (nueva_pos_i >= 0 && nueva_pos_i < f && nueva_pos_j >= 0 && nueva_pos_j < c) {
             pos_i[0] = nueva_pos_i;
             pos_j[0] = nueva_pos_j;
         }
+    }
+
+
+    void eliminar() {
+        int *antPos_i = 0;
+        int *antPos_j = 0;
+        delete[] antPos_i;
+        delete[] antPos_j;
+
     }
 
 };
@@ -273,7 +145,7 @@ public:
     }
 
     void generarFruta() {
-        miFruta.crearFruta(f,c);
+        miFruta.crearFruta(f, c);
         miMatriz[miFruta.pos_i][miFruta.pos_j] = miFruta.car_fruta;
     }
 
@@ -285,6 +157,11 @@ public:
     void moverGusano(char direccion) {
         miGusano.moverGusano(direccion, f, c);
         miMatriz[miGusano.pos_i[0]][miGusano.pos_j[0]] = miGusano.simbolo;
+    }
+
+    void eliminar() {
+        miGusano.eliminar();
+        miMatriz[miGusano.pos_i[0]][miGusano.pos_j[0]] = ' ';
     }
 
 };
@@ -310,13 +187,14 @@ int main()
         std::cin >> tecla;
         if (tecla == 'q' || tecla == 'Q') {
             isRun = false;
-        } else {
+        }
+        else {
             miTablero.moverGusano(tecla);
             miTablero.printMatriz();
+            miTablero.eliminar();
         }
     }
-
+    
     return 0;
 
 }
-
